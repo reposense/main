4f49b6bb73be49f679b2dd877ee047a869006e24
jordancjq <jordancjq@hotmail.com>
2018-03-15 12:22:13 +0800

    Logic: Update RemarkCommand to throw an exception

--- src/main/java/seedu/address/logic/commands/RemarkCommand.java
+++ src/main/java/seedu/address/logic/commands/RemarkCommand.java
@@ -1,4 +1,26 @@
 package seedu.address.logic.commands;
 
-public class RemarkCommand {
+import static seedu.address.logic.parser.CliSyntax.PREFIX_REMARK;
+
+import seedu.address.logic.commands.exceptions.CommandException;
+
+public class RemarkCommand extends UndoableCommand {
+
+    public static final String COMMAND_WORD = "remark";
+    public static final String COMMAND_ALIAS = "rm";
+
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Updates the remark of the person identified "
+            + "by the index number used in the last person listing. "
+            + "Existing values will be overwritten by the input values.\n"
+            + "Parameters: INDEX (must be a positive integer) "
+            + PREFIX_REMARK + "[REMARK]\n"
+            + "Example: " + COMMAND_WORD + " 1 "
+            + PREFIX_REMARK + "Must put on field";
+
+    public static final String MESSAGE_NOT_IMPLEMENTED = "Remark command not been implemented";
+
+    @Override
+    protected CommandResult executeUndoableCommand() throws CommandException {
+        throw new CommandException(MESSAGE_NOT_IMPLEMENTED);
+    }
 }
