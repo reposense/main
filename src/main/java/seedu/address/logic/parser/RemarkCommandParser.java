26df1d206d42732e9c9c1ca9707003132e5032c0
jordancjq <jordancjq@hotmail.com>
2018-03-15 12:58:21 +0800

    Logic: Add RemarkCommandParser to parse remark arguments

---
+++ src/main/java/seedu/address/logic/parser/RemarkCommandParser.java
@@ -0,0 +1,14 @@
+package seedu.address.logic.parser;
+
+import seedu.address.logic.commands.RemarkCommand;
+import seedu.address.logic.parser.exceptions.ParseException;
+
+/**
+ * Parses input arguments and creates a new RemarkCommand object
+ */
+public class RemarkCommandParser implements Parser<RemarkCommand> {
+
+    public RemarkCommand parse(String args) throws ParseException {
+        
+    }
+}
