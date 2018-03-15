12b6711de0e4c19a856d5fd76a6343141211920c
lithiumlkid <luvnenvy@gmail.com>
2018-03-15 20:08:25 +0800

    fix checkstyle

--- src/test/java/seedu/address/logic/AutocompleteTest.java
+++ src/test/java/seedu/address/logic/AutocompleteTest.java
@@ -12,18 +12,18 @@ public class AutocompleteTest {
     private CommandTrie commandTrie;
 
     @Before
-    public void setup(){
+    public void setup() {
         commandTrie = new CommandTrie();
     }
 
     @Test
-    public void autocomplete_unique_prefix(){
+    public void autocomplete_unique_prefix() {
         assert commandTrie.attemptAutoComplete("a").equals("add");
         assert commandTrie.attemptAutoComplete("he").equals("help");
     }
 
     @Test
-    public void autocomplete_multiple_prefix(){
+    public void autocomplete_multiple_prefix() {
         assert commandTrie.attemptAutoComplete("e").equals("e");
         assert commandTrie.attemptAutoComplete("H").equals("H");
 
