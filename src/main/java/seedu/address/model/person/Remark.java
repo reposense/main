17c4d00446ed5a617eab196c91902c16869883b7
jordancjq <jordancjq@hotmail.com>
2018-03-15 14:52:07 +0800

    Model: Update Remark class similar to Phone

--- src/main/java/seedu/address/model/person/Remark.java
+++ src/main/java/seedu/address/model/person/Remark.java
@@ -1,8 +1,42 @@
 package seedu.address.model.person;
 
+import static java.util.Objects.requireNonNull;
+
 /**
  * Represents a Person's remark in the address book.
  * Guarantees: immutable; is always valid}
  */
 public class Remark {
+
+    public static final String MESSAGE_REMARK_CONSTRAINTS =
+            "Remark can contain any values, can even be blank";
+
+    public final String value;
+
+    /**
+     * Constructs a {@code Remark}
+     *
+     * @param remark Any remark
+     */
+    public Remark(String remark){
+        requireNonNull(remark);
+        this.value = remark;
+    }
+
+    @Override
+    public String toString() {
+        return value;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return other == this // short circuit if same object
+                || (other instanceof Remark // instanceof handles nulls
+                && this.value.equals(((Remark) other).value)); // state check
+    }
+
+    @Override
+    public int hashCode() {
+        return value.hashCode();
+    }
 }
