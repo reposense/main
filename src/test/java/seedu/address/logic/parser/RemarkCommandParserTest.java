7da7ce052cf5d1b10e3c5b63b285a6b842855777
jordancjq <jordancjq@hotmail.com>
2018-03-15 13:27:33 +0800

    Test: Update RemarkCommandParserTest with test cases

--- src/test/java/seedu/address/logic/parser/RemarkCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/RemarkCommandParserTest.java
@@ -1,4 +1,44 @@
 package seedu.address.logic.parser;
 
+import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_REMARK;
+import static seedu.address.logic.parser.CommandParserTestUtil.assertParseFailure;
+import static seedu.address.logic.parser.CommandParserTestUtil.assertParseSuccess;
+import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_PERSON;
+
+import org.junit.Test;
+
+import seedu.address.logic.commands.RemarkCommand;
+
 public class RemarkCommandParserTest {
+
+    private static final String REMARK_EMPTY = "";
+    private static final String REMARK_NONEMPTY = "Some remark to test.";
+
+    private RemarkCommandParser parser = new RemarkCommandParser();
+
+    @Test
+    public void parse_indexSpecified_success() throws Exception {
+        // with remark
+        RemarkCommand expectedCommand = new RemarkCommand(INDEX_FIRST_PERSON, REMARK_NONEMPTY);
+        String userInput = INDEX_FIRST_PERSON.getOneBased() + " " + PREFIX_REMARK + " " + REMARK_NONEMPTY;
+        assertParseSuccess(parser, userInput, expectedCommand);
+
+        // without remark
+        expectedCommand = new RemarkCommand(INDEX_FIRST_PERSON, REMARK_EMPTY);
+        userInput = INDEX_FIRST_PERSON.getOneBased() + " " + PREFIX_REMARK + REMARK_EMPTY;
+        assertParseSuccess(parser, userInput, expectedCommand);
+    }
+
+    @Test
+    public void parse_compulsoryFieldMissing_failure() throws Exception {
+        String expectedMessage = String.format(MESSAGE_INVALID_COMMAND_FORMAT, RemarkCommand.MESSAGE_USAGE);
+
+        // missing parameters
+        assertParseFailure(parser, RemarkCommand.COMMAND_WORD, expectedMessage);
+
+        // missing index
+        assertParseFailure(parser, RemarkCommand.COMMAND_WORD + " " + REMARK_NONEMPTY, expectedMessage);
+    }
+
 }
