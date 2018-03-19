02b9cc76b72685bc0f901e6d4662d008f20c5b90
jordancjq <jordancjq@hotmail.com>
2018-03-19 17:55:26 +0800

    Model: Add Team and TeamName classes

--- src/main/java/seedu/address/model/team/Team.java
+++ src/main/java/seedu/address/model/team/Team.java
@@ -1,4 +1,53 @@
 package seedu.address.model.team;
 
+import java.util.Objects;
+
+import javafx.collections.ObservableList;
+import seedu.address.model.person.Person;
+import seedu.address.model.person.UniquePersonList;
+
+/**
+ * Represents a Team in the application.
+ * Guarantees: details are present and not null, field values are validated, immutable
+ */
 public class Team {
+
+    private final TeamName teamName;
+    private final UniquePersonList players;
+
+    /**
+     * Every field must be present and not null.
+     */
+    public Team(TeamName teamName) {
+        this.teamName = teamName;
+        this.players = new UniquePersonList();
+    }
+
+    public TeamName getTeamName() {
+        return teamName;
+    }
+
+    public ObservableList<Person> getTeamPlayers() {
+        return players.asObservableList();
+    }
+
+    @Override
+    public String toString() {
+        return teamName + ", " + players.asObservableList().size() + " players";
+        // TODO: refine later
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return other == this // short circuit if same object
+                || (other instanceof Team // instanceof handles nulls
+                && this.teamName.equals(((Team) other).teamName))
+                && this.players.equals(((Team) other).players);
+    }
+
+    @Override
+    public int hashCode() {
+        // use this method for custom fields hashing instead of implementing your own
+        return Objects.hash(teamName, players);
+    }
 }
---
+++ src/main/java/seedu/address/model/team/TeamName.java
@@ -0,0 +1,56 @@
+package seedu.address.model.team;
+
+import static java.util.Objects.requireNonNull;
+import static seedu.address.commons.util.AppUtil.checkArgument;
+
+/**
+ * Represents a Team's name in the application.
+ * Guarantees: imm utable; is valid as declared in {@link #isValidName(String)}
+ */
+public class TeamName {
+    public static final String MESSAGE_TEAM_NAME_CONSTRAINTS =
+            "Team name should only contain alphanumeric characters and spaces, and it should not be blank";
+
+    /*
+     * The first character of the team name must not be a whitespace,
+     * otherwise " " (a blank string) becomes a valid input.
+     */
+    public static final String NAME_VALIDATION_REGEX = "[\\p{Alnum}][\\p{Alnum} ]*";
+
+    public final String fullName;
+
+    /**
+     * Constructs a {@code TeamName}.
+     *
+     * @param teamName A valid team name.
+     */
+    public TeamName(String teamName) {
+        requireNonNull(teamName);
+        checkArgument(isValidName(teamName), MESSAGE_TEAM_NAME_CONSTRAINTS);
+        this.fullName = teamName;
+    }
+
+    /**
+     * Returns true if a given string is a valid team name.
+     */
+    public static boolean isValidName(String test) {
+        return test.matches(NAME_VALIDATION_REGEX);
+    }
+
+    @Override
+    public String toString() {
+        return fullName;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return other == this // short circuit if same object
+                || (other instanceof TeamName // instanceof handles nulls
+                && this.fullName.equals(((TeamName) other).fullName)); // state check
+    }
+
+    @Override
+    public int hashCode() {
+        return fullName.hashCode();
+    }
+}
