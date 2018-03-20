342022a12e1b296dbde96a0d6c171f93c9ed06a6
Tianwei <lohtianwei@msn.com>
2018-03-20 22:33:30 +0800

    Minor edit to sortCommand: Change usage example.
    Added model stub for sort function

--- src/main/java/seedu/address/logic/commands/SortCommand.java
+++ src/main/java/seedu/address/logic/commands/SortCommand.java
@@ -19,8 +19,8 @@ public class SortCommand extends UndoableCommand {
     public static final String MESSAGE_USAGE = COMMAND_WORD + ": Sorts players in address book "
             + "by fields in either ascending or descending order.\n"
             + "Parameters: KEYWORD [FIELD][ORDER]\n"
-            + "Example: " + COMMAND_WORD + " "
-            + PREFIX_NAME + BY_ASCENDING;
+            + "Example: " + COMMAND_WORD
+            + " name " + BY_ASCENDING;
 
     public static final String MESSAGE_SUCCESS = "Players in address book have been sorted.";
     public static final String MESSAGE_EMPTY_BOOK = "No player(s) to sort.";
