518adbccce6126250513c3ceb562fbfe19ce0232
Jordan Chong <jordancjq@hotmail.com>
2018-03-22 01:25:29 +0800

    Fix merge conflict typo

--- src/main/java/seedu/address/logic/parser/ParserUtil.java
+++ src/main/java/seedu/address/logic/parser/ParserUtil.java
@@ -174,6 +174,7 @@ public class ParserUtil {
         return Optional.of(value.orElse(UNSPECIFIED_FIELD));
     }
   
+    /**
      * Parses a {@code String tagColour} into a {@code String ta}.
      * Leading and trailing whitespaces will be trimmed.
      *
