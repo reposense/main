a334ec531a61061a28a9de2c6982f0a77214d7c5
Tianwei <lohtianwei@msn.com>
2018-03-22 23:03:36 +0800

    SortCommandParserTest: Removed redundant whitespaces

--- src/test/java/seedu/address/logic/parser/SortCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/SortCommandParserTest.java
@@ -23,58 +23,58 @@ public class SortCommandParserTest {
     @Test
     public void parse_invalidArguments_failure() {
         //more than 1 field entered
-        assertParseFailure(parser, " " + "name" + " " + "address" + " " + "asc",
+        assertParseFailure(parser, "name" + " " + "address" + " " + "asc",
                 String.format(MESSAGE_INVALID_COMMAND_FORMAT, MESSAGE_USAGE));
 
         //invalid field entered
-        assertParseFailure(parser, " " + "invalid" + "asc",
+        assertParseFailure(parser, "invalid" + "asc",
                 String.format(MESSAGE_INVALID_COMMAND_FORMAT, MESSAGE_USAGE));
 
         //invalid sort order entered
-        assertParseFailure(parser, " " + "name" + " " + "invalid",
+        assertParseFailure(parser, "name" + " " + "invalid",
                 String.format(MESSAGE_INVALID_COMMAND_FORMAT, MESSAGE_USAGE));
 
         //no field entered
-        assertParseFailure(parser, " " + "asc",
+        assertParseFailure(parser, "asc",
                 String.format(MESSAGE_INVALID_COMMAND_FORMAT, MESSAGE_USAGE));
 
         //no order entered
-        assertParseFailure(parser, " " + "name",
+        assertParseFailure(parser, "name",
                 String.format(MESSAGE_INVALID_COMMAND_FORMAT, MESSAGE_USAGE));
     }
 
     @Test
     public void parse_validArguments_success() {
         //valid input for sort by name in asc order
-        assertParseSuccess(parser, " " + "name" + " " + BY_ASCENDING,
+        assertParseSuccess(parser, "name" + " " + BY_ASCENDING,
                 new SortCommand("name", BY_ASCENDING));
 
         //valid input for sort by name in desc order
-        assertParseSuccess(parser, " " + "name" + " " + BY_DESCENDING,
+        assertParseSuccess(parser, "name" + " " + BY_DESCENDING,
                 new SortCommand("name", BY_DESCENDING));
 
         //valid input for sort by address in asc order
-        assertParseSuccess(parser, " " + "address" + " " + BY_ASCENDING,
+        assertParseSuccess(parser, "address" + " " + BY_ASCENDING,
                 new SortCommand("address", BY_ASCENDING));
 
         //valid input for sort by address in desc order
-        assertParseSuccess(parser, " " + "address" + " " + BY_DESCENDING,
+        assertParseSuccess(parser, "address" + " " + BY_DESCENDING,
                 new SortCommand("address", BY_DESCENDING));
 
         //valid input for sort by phone in asc order
-        assertParseSuccess(parser, " " + "phone" + " " + BY_ASCENDING,
+        assertParseSuccess(parser, "phone" + " " + BY_ASCENDING,
                 new SortCommand("phone", BY_ASCENDING));
 
         //valid input for sort by phone in desc order
-        assertParseSuccess(parser, " " + "phone" + " " + BY_DESCENDING,
+        assertParseSuccess(parser, "phone" + " " + BY_DESCENDING,
                 new SortCommand("phone", BY_DESCENDING));
 
         //valid input for sort by email in asc order
-        assertParseSuccess(parser, " " + "email" + " " + BY_ASCENDING,
+        assertParseSuccess(parser, "email" + " " + BY_ASCENDING,
                 new SortCommand("email", BY_ASCENDING));
 
         //valid input for sort by email in desc order
-        assertParseSuccess(parser, " " + "email" + " " + BY_DESCENDING,
+        assertParseSuccess(parser, "email" + " " + BY_DESCENDING,
                 new SortCommand("email", BY_DESCENDING));
     }
 }
