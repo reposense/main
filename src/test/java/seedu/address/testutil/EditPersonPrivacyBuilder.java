3debb89f556207d50fbede51b22b2dae534d29ed
Tianwei <lohtianwei@msn.com>
2018-04-05 08:47:32 +0800

    Checkstyle yet again

--- src/test/java/seedu/address/testutil/EditPersonPrivacyBuilder.java
+++ src/test/java/seedu/address/testutil/EditPersonPrivacyBuilder.java
@@ -2,6 +2,10 @@ package seedu.address.testutil;
 
 import seedu.address.logic.commands.TogglePrivacyCommand.EditPersonPrivacy;
 import seedu.address.model.person.Person;
+
+/**
+ * A utility class to help with building EditPersonPrivacy objects.
+ */
 public class EditPersonPrivacyBuilder {
 
     private EditPersonPrivacy epp;
@@ -13,6 +17,9 @@ public class EditPersonPrivacyBuilder {
         this.epp = new EditPersonPrivacy(epp);
     }
 
+    /**
+     * Returns an {@code EditPersonPrivacy} with fields containing {@code person}'s privacy details
+     */
     public EditPersonPrivacyBuilder(Person p) {
         epp = new EditPersonPrivacy();
         epp.setPrivateAddress(p.getAddress().isPrivate());
