621cadc7b0afded5311afd2b7a8f3d1ecfca3318
jordancjq <jordancjq@hotmail.com>
2018-04-12 05:42:10 +0800

    Fix: Logic of updating person

--- src/main/java/seedu/address/model/AddressBook.java
+++ src/main/java/seedu/address/model/AddressBook.java
@@ -129,7 +129,7 @@ public class AddressBook implements ReadOnlyAddressBook {
         // TODO: the tags master list will be updated even though the below line fails.
         // This can cause the tags master list to have additional tags that are not tagged to any person
         // in the person list.
-        if (!target.getTeamName().toString().equals(UNSPECIFIED_FIELD)) {
+        if (!editedPerson.getTeamName().toString().equals(UNSPECIFIED_FIELD)) {
             teams.getTeam(editedPerson.getTeamName()).setPerson(target, editedPerson);
         }
         persons.setPerson(target, syncedEditedPerson);
